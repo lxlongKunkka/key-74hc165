35735266b52b6b0e6ae074564ddf8db766cc21d6
lxlongKunkka <110076790@qq.com>
2022-04-14 10:16:54 +0800

    Update simpleShield74HC165.ts

--- simpleShield74HC165.ts
+++ simpleShield74HC165.ts
@@ -23,22 +23,22 @@ namespace SimpleShieldKey
         let val = 0;
         
         pins.digitalWritePin(INSR_LATCH, 0);    //scan
-        control.waitMicros(1000);
+        //control.waitMicros(1000);
         pins.digitalWritePin(INSR_LATCH, 1);
         let i = 0;
         for(i=0; i<8; i++)
         {
-            basic.showNumber(i);
-            control.waitMicros(1000000);
+            //basic.showNumber(i);
+            //control.waitMicros(1000000);
             val = val << 1;
             let tmp = pins.digitalReadPin(INSR0_DATA);
-            basic.showNumber(tmp);
+            //basic.showNumber(tmp);
             val |= tmp;
-            control.waitMicros(1000);
+            //control.waitMicros(1000);
             pins.digitalWritePin(SR_CLK, 0);
-            control.waitMicros(1000);
+            //control.waitMicros(1000);
             pins.digitalWritePin(SR_CLK, 1);
-            control.waitMicros(1000);
+            //control.waitMicros(1000);
         }
         basic.showNumber(val);
         control.waitMicros(2000000);
